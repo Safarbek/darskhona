b7861e785add4bea7bc7b14035aefaa23235802b
UmarovSafar <tojikacademy>
2020-10-10 10:52:12 +0700

    For youT

---
+++ README.md
@@ -0,0 +1,3 @@
+# frontentProjects
+
+1. <h3>You can see this project bankAcademy in browser<a href='https://umarovsafarbek.github.io/frontentProjects/bankAcademy/index.html'> Click here to see </a></h3>
---
+++ englishTjAcademy/lessons/js/main.js
@@ -0,0 +1,50 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const list = document.querySelectorAll(".nav-links li")
+    
+    burger.addEventListener("click", () =>{ 
+        
+        nav.classList.toggle("nav-active")
+      
+
+        list.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+                return ''
+            }
+            link.style.animation = `navLink 0.5s ease forwards ${index / 7+1.2}s`;
+        }) 
+
+
+        // burger animation
+
+        burger.classList.toggle("toggel")
+    })
+
+
+   
+}
+navSlide()
+
+// body animation
+
+
+
+
+// Show pronoun sentences test
+
+var showPron  = document.querySelector(".showPronounSent");
+showPron.addEventListener("click", (e) => {
+    var sent = document.querySelector('.sentenceWithP');
+    if(sent.style.display == "none") {
+        sent.style.display = 'block'
+        e.target.innerText = 'Пушидани ҷавоб!'
+    } else {
+        sent.style.display = 'none'
+        e.target.innerText = 'Намоиши ҷавоб!'
+
+    }
+})
+
+
